9769f1eb25a2f6baef0f2c868b49e6cbf0af53a9
bmuppa <106985590+bmuppa@users.noreply.github.com>
2022-06-26 13:20:38 -0500

    Add files via upload

---
+++ lab03.js
@@ -0,0 +1,15 @@
+document.getElementById('kginput').addEventListener('input', function(e) {
+    let pounds = e.target.value;
+    document.getElementById('poundsoutput').innerHTML = pounds / 0.0022046;
+    document.getElementById('kilosoutput').innerHTML = pounds;
+});
+document.getElementById('kmsinput').addEventListener('input', function(a) {
+    let miles = a.target.value;
+    document.getElementById('milesoutput').innerHTML = miles * 0.62137;
+    document.getElementById('kilometeresoutput').innerHTML = miles;
+});
+document.getElementById('faranheitinput').addEventListener('input', function(u) {
+    let celsius = u.target.value;
+    document.getElementById('celsiusoutput').innerHTML = (((celsius - 32) * 5) / 9);
+    document.getElementById('faranheitoutput').innerHTML = celsius;
+});
